9032daf30c560ffca6a4f7e9c994257cb3859fe3
Ricco Kaints <ricgz@Riccos-Laptop.local>
2026-08-03 08:37:06 +0300

    Add edit item feature

--- app.py
+++ app.py
@@ -34,6 +34,21 @@ def create_item():
 
     return redirect("/")
 
+@app.route("/edit_item/<int:item_id>")
+def edit_item(item_id):
+    item = items.get_item(item_id)
+    return render_template("edit_item.html", item=item)
+
+@app.route("/update_item", methods=["POST"])
+def update_item():
+    item_id = request.form["item_id"]
+    title = request.form["title"]
+    description = request.form["description"]
+
+    items.update_item(item_id, title, description)
+
+    return redirect("/item/" + str(item_id))
+
 @app.route("/register")
 def register():
     return render_template("register.html")
