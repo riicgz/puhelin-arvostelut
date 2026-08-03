4a1bfbd95043603710cfc17463e65f36c3c41d96
Ricco Kaints <ricgz@Riccos-Laptop.local>
2026-08-03 09:04:34 +0300

    Add find item feature

--- items.py
+++ items.py
@@ -18,3 +18,21 @@ def get_item(item_id):
              WHERE items.user_id = users.id AND
                    items.id = ?"""
     return db.query(sql, [item_id])[0]
+
+def update_item(item_id, title, description):
+    sql = """UPDATE items SET title = ?,
+                              description = ?
+                          WHERE id = ?"""
+    db.execute(sql, [title, description, item_id])
+
+def remove_item(item_id):
+    sql = "DELETE FROM items WHERE id = ?"
+    db.execute(sql, [item_id])
+
+def find_items(query):
+    sql = """SELECT id, title
+             FROM items
+             WHERE title LIKE ? OR description LIKE ?
+             ORDER BY id DESC"""
+    like = "%" + query + "%"
+    return db.query(sql, [like, like])
