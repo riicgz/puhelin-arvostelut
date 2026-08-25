0abbcf0039b89e724d277e5ab108aa39cfdff1ad
Ricco Kaints <ricgz@MacBookPro.home>
2026-08-25 17:57:59 +0300

    Add classes to edit page

--- app.py
+++ app.py
@@ -83,7 +83,15 @@ def edit_item(item_id):
         abort(404)
     if item["user_id"] != session["user_id"]:
         abort(403)
-    return render_template("edit_item.html", item=item)
+
+    all_classes = items.get_all_classes()
+    classes = {}
+    for my_class in all_classes:
+        classes[my_class] = ""
+    for entry in items.get_classes(item_id):
+        classes[entry["title"]] = entry["value"]
+
+    return render_template("edit_item.html", item=item, classes=classes, all_classes=all_classes)
 
 @app.route("/update_item", methods=["POST"])
 def update_item():
@@ -101,7 +109,13 @@ def update_item():
     if not description or len(description) > 2000:
         abort(403)
 
-    items.update_item(item_id, title, description)
+    classes = []
+    for entry in request.form.getlist("classes"):
+        if entry:
+            parts = entry.split(":")
+            classes.append((parts[0], parts[1]))
+    
+    items.update_item(item_id, title, description, classes)
 
     return redirect("/item/" + str(item_id))
 
--- items.py
+++ items.py
@@ -43,11 +43,18 @@ def get_item(item_id):
     result = db.query(sql, [item_id])[0]
     return result[0] if result else None
 
-def update_item(item_id, title, description):
+def update_item(item_id, title, description, classes):
     sql = """UPDATE items SET title = ?,
                               description = ?
                           WHERE id = ?"""
     db.execute(sql, [title, description, item_id])
+
+    sql = "DELETE FROM item_classes WHERE item_id = ?"
+    db.execute(sql, [item_id])
+
+    sql = "INSERT INTO item_classes (item_id, title, value) VALUES (?, ?, ?)"
+    for title, value in classes:
+        db.execute(sql, [item_id, title, value])
 
 def remove_item(item_id):
     sql = "DELETE FROM items WHERE id = ?"
