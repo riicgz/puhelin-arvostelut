2c19f00d5e71b6868871ef3201ddf804e30e0ca8
Ricco Kaints <ricgz@MacBookPro.home>
2026-08-27 00:38:15 +0300

    Don't allow user to change classes

--- app.py
+++ app.py
@@ -65,11 +65,17 @@ def create_item():
         abort(403)
     user_id = session["user_id"]
 
+    all_classes = items.get_all_classes()
+
     classes = []
     for entry in request.form.getlist("classes"):
         if entry:
-            parts = entry.split(":")
-            classes.append((parts[0], parts[1]))
+            class_title, class_value = entry.split(":")
+            if class_title not in all_classes:
+                abort(403)
+            if class_value not in all_classes[class_title]:
+                abort(403)
+            classes.append((class_title, class_value))
 
     items.add_item(title, description, verdict, user_id, classes)
 
@@ -109,11 +115,17 @@ def update_item():
     if not description or len(description) > 2000:
         abort(403)
 
+    all_classes = items.get_all_classes()
+    
     classes = []
     for entry in request.form.getlist("classes"):
         if entry:
-            parts = entry.split(":")
-            classes.append((parts[0], parts[1]))
+            class_title, class_value = entry.split(":")
+            if class_title not in all_classes:
+                abort(403)
+            if class_value not in all_classes[class_title]:
+                abort(403)
+            classes.append((class_title, class_value))
     
     items.update_item(item_id, title, description, classes)
 
